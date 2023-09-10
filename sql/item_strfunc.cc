0f925a83fbcbf9740594537837ad01adac3cda29
Sergei Golubchik <serg@mariadb.org>
2023-09-10 20:10:53 +0200

    compile-time deprecation reminders
    remove old deprecation helpers that were not used anywhere.
    
    create new deprecation helpers and enforce their usage
    
    this also removes inconsistencies in reporting deprecation:
    sometimes it was ER_WARN_DEPRECATED_SYNTAX (1287),
    sometimes ER_WARN_DEPRECATED_SYNTAX_NO_REPLACEMENT (1681),
    sometimes a warning, sometimes a note.
    
    it should always be
    * ER_WARN_DEPRECATED_SYNTAX
    * a warning (because it's something actionable, not purely informational)

--- sql/item_strfunc.cc
+++ sql/item_strfunc.cc
@@ -905,9 +905,7 @@ bool Item_func_des_encrypt::fix_length_and_dec(THD *thd)
   set_maybe_null();
   /* 9 = MAX ((8- (arg_len % 8)) + 1) */
   max_length = args[0]->max_length + 9;
-  push_warning_printf(thd, Sql_condition::WARN_LEVEL_NOTE, ER_WARN_DEPRECATED_SYNTAX,
-                      ER_THD(thd, ER_WARN_DEPRECATED_SYNTAX_NO_REPLACEMENT),
-                      func_name_cstring().str); // since 10.10.1
+  warn_deprecated<1010>(thd, func_name_cstring().str);
   return FALSE;
 }
 
@@ -1018,9 +1016,7 @@ bool Item_func_des_decrypt::fix_length_and_dec(THD *thd)
   max_length= args[0]->max_length;
   if (max_length >= 9U)
     max_length-= 9U;
-  push_warning_printf(thd, Sql_condition::WARN_LEVEL_NOTE, ER_WARN_DEPRECATED_SYNTAX,
-                      ER_THD(thd, ER_WARN_DEPRECATED_SYNTAX_NO_REPLACEMENT),
-                      func_name_cstring().str); // since 10.10.1
+  warn_deprecated<1010>(thd, func_name_cstring().str);
   return FALSE;
 }
 
@@ -2714,7 +2710,6 @@ String *Item_func_encrypt::val_str(String *str)
 
 bool Item_func_encode::seed()
 {
-  THD *thd= current_thd;
   char buf[80];
   ulong rand_nr[2];
   String *key, tmp(buf, sizeof(buf), system_charset_info);
@@ -2725,9 +2720,7 @@ bool Item_func_encode::seed()
   hash_password(rand_nr, key->ptr(), key->length());
   sql_crypt.init(rand_nr);
 
-  push_warning_printf(thd, Sql_condition::WARN_LEVEL_WARN, ER_WARN_DEPRECATED_SYNTAX,
-                      ER_THD(thd, ER_WARN_DEPRECATED_SYNTAX_NO_REPLACEMENT),
-                      func_name_cstring().str); // since 11.3
+  warn_deprecated<1103>(current_thd, func_name_cstring().str);
 
   return FALSE;
 }
